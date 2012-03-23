4e21282ddfe8990083d53cb1637875d01eef2f52
Xavier Riley <xavier@kyanmedia.com>
2012-03-23 10:31:54 +0000

    Readded tiny mce and css for image upload

--- templates/app/helpers/kyan_form_builder.rb
+++ templates/app/helpers/kyan_form_builder.rb
@@ -28,6 +28,58 @@ class Padrino::Helpers::FormBuilder::KyanFormBuilder < Padrino::Helpers::FormBui
   end
 
   def text_mce_block(field, options={}, label_options={})
+    label_options.reverse_merge!(:caption => options.delete(:caption)) if options[:caption]
+    css_class_options = prepare_class_options(options[:class], ['text_area', field.to_s])
+    field_html = label(field, label_options)
+    field_html << error_message_on(field)
+    field_html << <<-EOF
+      <script type="text/javascript">
+        $(function() {
+          $('\##{@object.class.name.demodulize.underscore}_#{field}').tinymce({
+            // Location of TinyMCE script
+            script_url : '/admin/javascripts/tiny_mce/tiny_mce.js',
+
+            // General options
+            mode : "textareas",
+            theme : "advanced",
+            plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+            // Theme options
+            theme_advanced_buttons1 : "code,undo,redo,|,bold,italic,underline,|,styleselect,formatselect,|,bullist,numlist",
+            theme_advanced_buttons2 : "",
+            theme_advanced_buttons3 : "",
+            theme_advanced_buttons4 : "",
+            //theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+            //theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+            //theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+            theme_advanced_toolbar_location : "top",
+            theme_advanced_toolbar_align : "left",
+            theme_advanced_statusbar_location : "bottom",
+            theme_advanced_resizing : true,
+
+            // Example content CSS (should be your site CSS)
+            content_css : "/stylesheets/core.css",
+
+            // Drop lists for link/image/media/template dialogs
+            template_external_list_url : "js/template_list.js",
+            external_link_list_url : "js/link_list.js",
+            external_image_list_url : "js/image_list.js",
+            media_external_list_url : "js/media_list.js",
+
+            // Replace values for the template plugin
+            template_replace_values : {
+                    username : "Some User",
+                    staffid : "991234"
+            }
+          });
+        });
+      </script>
+    EOF
+    field_html << text_area(field.to_sym, :class => css_class_options)
+    @template.content_tag(:div, field_html)
+  end
+
+  def text_mce_block(field, options={}, label_options={})
       label_options.reverse_merge!(:caption => options.delete(:caption)) if options[:caption]
       css_class_options = prepare_class_options(options[:class], ['text_area', field.to_s])
       field_html = label(field, label_options)
