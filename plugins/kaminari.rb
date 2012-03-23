4f9b14a3246bcdd689d9d398b9cbf1ae9f59280f
Xavier Riley <xavier@kyanmedia.com>
2012-03-23 13:37:54 +0000

    Added kaminari views and plugin

---
+++ plugins/kaminari.rb
@@ -0,0 +1,15 @@
+
+KAMINARI_PATCH = <<-KAMINARI_PATCH
+class Kaminari::Helpers::SinatraHelpers::ActionViewTemplateProxy
+  def render(*args)
+    base = ActionView::Base.new.tap do |a|
+      a.view_paths << File.expand_path(Padrino.root('app/views'), __FILE__)
+      a.view_paths << File.expand_path('../../../../app/views', __FILE__)
+    end
+    base.render(*args)
+  end
+end
+KAMINARI_PATCH
+
+create_file "lib/kaminari_override", KAMINARI_PATCH
+directory "templates/erb/app/kaminari",        destination_root("app", "views")
